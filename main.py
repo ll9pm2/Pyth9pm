bfe1093c34532dc3f2a15c66ac2cf905d46f4c71
ll9pm2 <aboo.dll9.pm@gmail.com>
2025-08-08 21:57:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ async def main():
     """Creates an aiohttp session and fetches multiple URLs concurrently."""
     urls = list()
     apis =[
-        "hdcyXyvv7Ret0Hl4ZXpGnsgXKCbsK1QZ","8K9kwA8G5NbCjS4K9X0bKrKCfApgnyoA"]
+        "9iZ2ifRSNdA7Crxudr8xW4RPGYiloEsR","Sf1NdBMAu2nT0yvi6iLlqYMsGavHdvkv"]
     #ad ="https://www.effectiveratecpm.com/ca83bzpx98?key=dee9c6f3171b614287718132222041ad"
     #ad = "https://www.profitableratecpm.com/zhzbtigdvk?key=bfdf77a1bedb6a88e866ad888aa3896b"
     ad ="https://www.profitableratecpm.com/mrrsnt73cz?key=a9264f3ab1770607dd37951c2da5963c"
