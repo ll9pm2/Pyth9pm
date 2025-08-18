a8cae8af833b0615ed91d2049dc5cf3c3af428d8
ll9pm2 <aboo.dll9.pm@gmail.com>
2025-08-18 12:45:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ async def main():
     urls = list()
     apis =[
         "xRay19CvcTHL5gI0lj5tudj2E0qZOUqE","l8A2d1gMDXoLPePZ9uGdWytGxlIgAHj5"]
-    ad="https://viidedss.com/dc/?blockID=382755"
+    ad="https://viidedss.com/dc/?blockID=383089"
     for api in apis:
         for _ in range(randint (3,5)):
             #url ="https://api.webscrapingapi.com/v1?api_key=jjgCEsYztVo8MpvwtZ2rVqZxMwTKQXFo&url=https%3A%2F%2Fwww.profitableratecpm.com%2Fes8iaffr0n%3Fkey%3Da180891d7e00848a91909a7b8081d758&render_js=1&wait_until=networkidle0&wait_for=10000&country=il"
