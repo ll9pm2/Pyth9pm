5a608f0b53316303290981b5dc989c06107f418a
ll9pm2 <aboo.dll9.pm@gmail.com>
2025-10-12 11:41:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,18 +27,7 @@ def send_request(api):
 for i in range(110):
 	# Create a list of threads
 	threads = []
-	apis = ["338c9f49-d8b7-469d-84ce-d637ef751d15",
-	"21f8d9ba-186b-4a75-845a-bf8b5885292e",
-	"bf10ce57-122a-41be-8ae3-d7ae1ccfd0fd",
-	"cca17caa-126b-46e9-902b-68b1bc1ed473",
-	"0c8b6ab6-183a-4506-9650-707c68c0751e",
-	"3a1aeb60-e567-40f2-9705-e44ba717374e",
-	"56e05ac3-f9aa-407c-b792-3c2b45858e10",
-	"2a46b0a9-968b-4e96-98fc-148ca58fab64",
-	"14337553-7293-4edf-af18-0bbc9ae36165",
-	"3071f8e3-109d-4514-ab9d-592550c2d595",
-	"b2cafe42-4df4-4e98-93b9-2397b8e03b03"
-	] 
+	apis = ['bd800d03-4239-423f-8530-ff54d19ece80', '83d9ba38-560c-4d0a-87bf-1746bedcadcd', '1e1cf78a-cd54-4882-9e71-e6bfb50ee79e', '6b06907a-72a7-4149-a6fe-2ec6a361187a', '3f747307-4667-41a8-af27-9fdae76071db', '91557644-4e49-4176-8e8a-566791f3298c', 'f64e734b-f115-479a-af06-ddd248d6bc15', 'de3e94cc-16dc-4eb6-9d59-3ff7271dd8e2', '43accce1-5208-44a6-a8d1-cd5229da08b4', 'db066a02-de89-4213-91ca-7080a7bc2e50']
 	
 	for api in apis:
 	    thread = threading.Thread(target=send_request, args=(api,))
