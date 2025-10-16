efb8b49ef5e61b64f1e53720e2e698cd7e273908
ll9pm2 <aboo.dll9.pm@gmail.com>
2025-10-16 15:18:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ if __name__ == '__main__':
     for i in range(1):
 
       apis =[
-"ab6k0pxo4LQSm0eqzUoaSHhJS12jxcvYI2pOUJOsrSgKbqJisnE19UMh8oWk",
+"o2DBj1IYXFYy0ycRDFWR3tKAqsBxQZ9qiuWkEfnWRWIRYMro0EdxP8mutzYN",
 #"foFYfHyndG3ev44yZjoaeRQanTqpHeHZyk8UbxmpFKacddWZYmDW6aK19pXG",
 #"WIOxjWiR89XI9gNyrFajmZo4UB48Agntk3zcaoTHqYbxgYNpuw1qvSo4LGeV",
 #"V0y3PHKMCHPEljlPsuDzcQdiL2Cpu36e5Zg2Cz3O5ip5SCa0KXoCq9ebrWQ9",
