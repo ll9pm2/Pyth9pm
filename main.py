01569b4812e1615cd0bae0bb02b90d1d73638c23
ll9pm2 <aboo.dll9.pm@gmail.com>
2025-12-01 05:49:52 +0300

    Increase concurrent requests and update API calls

--- main.py
+++ main.py
@@ -1,63 +1,59 @@
-import requests
 # Install the Python Requests library:
 # `pip install requests`
 import requests
 import threading
 import time
-from random import randint
+from time import sleep
 # Define the number of requests you want to send concurrently
-NUM_REQUESTS = 1
+NUM_REQUESTS = 2
+    
+    # Replace with your actual proxy details
+    
+
+    # URL-encode the password if it contains special characters like '@', ':', '%'
 
 def send_request(request_number,api):
     """
     Sends a single HTTP GET request and prints the status and content.
     The 'request_number' is used for clearer identification in the output.
     """
-    headers = {
-    'Content-Type': 'application/json',
-    'x-api-key': api,
-}
-
-    json_data = {
-    'url': 'https://viikqoye.com/dc/?blockID=399115',
-    'proxyType': 'residential',
-    'proxyCountry': 'US',
-    'blockResources': False,
-    'blockAds': False,
-    'blockUrls': [],
-    'wait': randint(10000,25000),
-    'jsScenario': [
-        {
-            'click': 'button',
-        },
-        {
-            'wait': f"{randint(1000,5000)}",
-        },
-    ],
-    'extractRules': {
-        'title': 'h1',
-    },
-    'screenshot': True,
-    'jsRendering': True,
-    'extractEmails': True,
-    'includeOnlyTags': [],
-    'excludeTags': [],
-    'outputFormat': [],
-}
-
-
+    #token = api
+#    url="https://viikqoye.com/dc/?blockID=394255"
+# 
     start_time = time.time()
     thread_name = threading.current_thread().name
     print(f"[{time.strftime('%H:%M:%S')}] {thread_name}: Starting Request #{request_number}...")
 
     try:
-        response = requests.post('https://api.hasdata.com/scrape/web', headers=headers, json=json_data)
-
-        print(response.text)
-      
+        #response = requests.post( "https://api.scrapeless.com/api/v2/unlocker/request",
+#       json=payload,
+#       headers={
+#           "x-api-token": token,
+#           "Content-Type": "application/json"
+#       },
+#       timeout=60)
+        proxy_username = "fwEiEZznyiwskSYg"
+        proxy_password = "G0JV6SJbj9R2SnNu" # Be careful with special characters in passwords 
+        proxy_ip = "geo.floppydata.com"
+        proxy_port = "10080"
+        proxies = {
+        "http": f"http://{proxy_username}:{proxy_password}@{proxy_ip}:{proxy_port}",
+        #"https": f"https://{proxy_username}:{proxy_password}@{proxy_ip}:{proxy_port}",
+    }
+        response = requests.get(
+            url='https://api.webscrapingapi.com/v2?',
+            params={
+                'api_key': api,
+                'url': "https://www.effectivegatecpm.com/wi8c609y28?key=433b0fd1f7990464917c15b1a2e1be90",
+                "render_js":"1"
+            },
+            
+            timeout=60 # Set a timeout to prevent threads from hanging indefinitely
+        )
+        
         duration = time.time() - start_time
         print(f"[{time.strftime('%H:%M:%S')}] {thread_name}: Finished Request #{request_number}. Status Code: {response.status_code} in {duration:.2f}s")
-        # print('Response Body: ', response.content) # Uncomment to see the content
+        print('Response Body: ',"proxy" in response.text) # Uncomment to see the content
     
     except requests.exceptions.RequestException as e:
         print(f"[{time.strftime('%H:%M:%S')}] {thread_name}: Error in Request #{request_number}: {e}")
@@ -89,16 +85,13 @@ def send_requests_with_threading(api):
     print(f"Total elapsed time: {global_duration:.2f} seconds")
 
 if __name__ == '__main__':
-    for i in range(200):
+    for i in range(3000):
       
-      apis =[
-     "57acce2c-9129-4105-989d-330f88dd97ec",
-"4bb5a5ae-e930-4362-9e98-c5e9daad3bd9",
-"2cf15792-0ed4-4611-8e41-dcc405d0b27a",
-"d778c504-8b36-4b4f-a14a-7799a52b9d95",
-"b535c470-d704-4b29-b758-cff276c2eb67",
-"0733584e-0e95-46bf-9995-4cca8d820fe8",
-         ]
+      apis =["9jnK3XQJHBNf8nr3u7zZ2gj6QNyuYujo",
+    ]
+      
+    #  proxy = f"http://oc-3e23d2c0fdc45df7035adb7639613b0b70bcc0177590cade0f3c1b84bdf0ffa8-country-us-session-5{i}6gg{i+2}hh{i*3}b:uqx15udtl5ix@proxy.oculus-proxy.com:31114"
+      #proxy="https://spgcjphbu4:Hy+zH5s4vmfr3vS3iN@gate.decodo.com:10001"
       threads2 = []
       for api in apis:
         # Create a new Thread object, targeting the send_request function
@@ -107,8 +100,3 @@ if __name__ == '__main__':
         thread2.start() # Immediately start the thread, running the request concurrently
       for thread2 in threads2:
         thread2.join()
-    
-
-      
-
-
